2fad62c27b76ee0f8f096f79ef4207709101624e
Wiktor <watulewicz@gmail.com>
2023-11-06 19:55:48 +0100

    Added working gameboard array with updateBoard function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { useEffect, useState } from "react";
+import GameBoard from "./components/gameBoard";
+
 function App() {
-  return <div>App</div>;
+  //using useState to create initial board that will change during game
+  const [board, setBoard] = useState(() => {
+    //using loop to create 5 rows, 5 cells gameboard
+    const initialBoard = [];
+    for (let i = 0; i < 5; i++) {
+      initialBoard.push(Array(5).fill(""));
+    }
+    return initialBoard;
+  });
+
+  //function used to update cells in gameboard,
+  // you need to pass row number, then cell number, and value
+  const updateBoard = (rowNumber, cellNumber, value) => {
+    const updatedBoard = board.map((row, rowIndex) => {
+      if (rowIndex === rowNumber) {
+        return row.map((cell, cellIndex) => {
+          if (cellIndex === cellNumber) {
+            return value;
+          } else {
+            return cell;
+          }
+        });
+      } else {
+        return row;
+      }
+    });
+    return updatedBoard;
+  };
+
+  //temporarily using handleClick to test working of updateBoard function
+  //It works :)
+  const handleClick = () => {
+    setBoard(updateBoard(0, 3, "A"));
+  };
+  //useEffect to see any changes after update
+  useEffect(() => {
+    console.log(board);
+  }, [board]);
+
+  return (
+    <div>
+      <button onClick={handleClick}>zmień pierwszy element na A</button>
+      <GameBoard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/gameBoard.js
@@ -0,0 +1,5 @@
+function GameBoard() {
+  return <div>Game board</div>;
+}
+
+export default GameBoard;
