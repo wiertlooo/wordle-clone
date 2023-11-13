898b073460568e7a8b3bae0509ea1e3a226a21e0
Wiktor <watulewicz@gmail.com>
2023-11-13 13:29:43 +0100

    added keyboard ref, keyboard handlers, default focus

--- src/components/gameKeyboard.js
+++ src/components/gameKeyboard.js
@@ -1,17 +1,41 @@
+import { useEffect, useRef } from "react";
+
 function GameKeyboard() {
   const keyLayout = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
     ["Z", "X", "C", "V", "B", "N", "M"],
-    ["↵", "⌫"],
+    ["BACKSPACE", "ENTER"],
   ];
+  const keyboardRef = useRef(null);
+
+  useEffect(() => {
+    if (keyboardRef.current) {
+      keyboardRef.current.focus();
+    }
+  }, []);
+
+  const handleKeyDown = (event) => {
+    const key = event.key.toUpperCase();
+    console.log(key);
+  };
+
+  const handleKeyPress = (key) => {
+    console.log(`Pressed key: ${key}`);
+  };
 
   const keyboard = keyLayout.map((keyRow, keyRowIndex) => {
     return (
       <div key={keyRowIndex} className="keyRow">
         {keyRow.map((key, keyIndex) => {
           return (
-            <div key={keyIndex} className="keyTile">
+            <div
+              key={keyIndex}
+              className="keyTile"
+              data-key={key}
+              onClick={() => handleKeyPress(key)}
+              tabIndex="0"
+            >
               {key}
             </div>
           );
@@ -20,7 +44,11 @@ function GameKeyboard() {
     );
   });
 
-  return <div>{keyboard}</div>;
+  return (
+    <div tabIndex="0" onKeyDown={handleKeyDown} ref={keyboardRef}>
+      {keyboard}
+    </div>
+  );
 }
 
 export default GameKeyboard;
